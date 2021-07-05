4d3472b4e91a9bd735d4ff898457ff9e09a1815d
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-07-05 12:33:38 +0500

    bdseed
    dbseed

--- database/seeds/UnitSeeder.php
+++ database/seeds/UnitSeeder.php
@@ -15,13 +15,57 @@ class UnitSeeder extends Seeder
         DB::table('units')->insert([
             [
                 'unit_name' => 'Name 1',
-                'unit_price' => '1000',
+                'unit_price' => '5000',
                 'property_id' => '6',
                 'ownership_id' => '3',
                 'property_title' => 'Demo Title 1',
                 'status' => '0',
                 'current' => '1',
-                'created_at' => '2021-07-03 04:07:31'
+                'created_at' => '2021-01-03 04:07:31'
+            ],
+
+            [
+                'unit_name' => 'Name 2',
+                'unit_price' => '4000',
+                'property_id' => '6',
+                'ownership_id' => '3',
+                'property_title' => 'Demo Title 2',
+                'status' => '0',
+                'current' => '1',
+                'created_at' => '2021-02-03 04:07:31'
+            ],
+
+            [
+                'unit_name' => 'Name 1',
+                'unit_price' => '2000',
+                'property_id' => '6',
+                'ownership_id' => '3',
+                'property_title' => 'Demo Title 1',
+                'status' => '0',
+                'current' => '1',
+                'created_at' => '2021-03-03 04:07:31'
+            ],
+
+            [
+                'unit_name' => 'Name 1',
+                'unit_price' => '7000',
+                'property_id' => '6',
+                'ownership_id' => '3',
+                'property_title' => 'Demo Title 1',
+                'status' => '0',
+                'current' => '1',
+                'created_at' => '2021-04-03 04:07:31'
+            ],
+
+            [
+                'unit_name' => 'Name 1',
+                'unit_price' => '10000',
+                'property_id' => '6',
+                'ownership_id' => '3',
+                'property_title' => 'Demo Title 1',
+                'status' => '0',
+                'current' => '1',
+                'created_at' => '2021-05-03 04:07:31'
             ],
 
             [
