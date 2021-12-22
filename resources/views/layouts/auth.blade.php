21405bdafa2c05ab51da077239795ba6a5c0737f
ali-hassan1-122 <ah.alihassan09@gmail.com>
2021-12-22 19:35:53 +0500

    new
    new

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Admin | Dashboard</title>
+    <title>Admin | Login</title>
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet"
